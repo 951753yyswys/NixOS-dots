268529b5a38fbedafbda1262039389abce271754
Qaaxaap <lkn15289353760@126.com>
2026-02-10 19:34:41 +0800

    fix:QQ 输入法爆炸

--- home.nix
+++ home.nix
@@ -2,5 +2,17 @@
 
 {
   home-manager.users.Qaaxaap = { pkgs, ... }: {
+    nixpkgs.config.allowUnfree = true;
+    xdg.dataFile."applications/qq.desktop".text = ''
+      [Desktop Entry]
+      Name=QQ
+      Exec=qq --ozone-platform=wayland --enable-wayland-ime --wayland-text-input-version=3 %U
+      Terminal=false
+      Type=Application
+      Icon=${pkgs.qq}/share/icons/hicolor/512x512/apps/qq.png
+      StartupWMClass=QQ
+      Categories=Network;
+      Comment=QQ
+    '';
   };
 }
--- zsh.nix
+++ zsh.nix
@@ -1,40 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  # 启用 zsh
   programs.zsh.enable = true;
-  
-  # 设置默认 shell
   users.users.Qaaxaap = {
     shell = pkgs.zsh;
   };
-  
-  # 允许系统使用 zsh
   environment.shells = with pkgs; [ bash zsh ];
   users.defaultUserShell = pkgs.zsh;
-  # 通过 home-manager 配置用户 zsh
   home-manager.users.Qaaxaap = { pkgs, ... }: {
     home.file.".p10k.zsh".source = ./p10k.zsh;
     home.packages = with pkgs; [
       zsh-powerlevel10k
     ];
-    
     programs.zsh = {
       enable = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
-      
       oh-my-zsh = {
         enable = true;
         plugins = [ "git" "sudo" ];
         theme = "";
       };
-      
       initContent = ''
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
       '';
     };
-    
   };
 }
